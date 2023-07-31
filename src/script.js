0e29c2a3c166d328b074060d69048d7925bbadb0
Fid <38692385+fidken@users.noreply.github.com>
2023-07-31 13:45:07 +0300

    88888

--- src/script.js
+++ src/script.js
@@ -1,19 +1,22 @@
 const http = require("http");
+const fs = require('fs');
 
-const host = 'localhost';
-const port = 8010;
-
-const requestListener = function (req, res) {
-    res.writeHead(200);
-    res.end("./index.html");
-};
-
-const server = http.createServer(requestListener);
-server.listen(port, host, () => {
-    console.log(`Server is running on http://${host}:${port}`);
-});
-
-function bruh() {
-    res.writeHead(200);
-    res.end("3d9e8599");
-}
+const server = http.createServer((req, res) => {
+    // Чтение файла index.html
+    fs.readFile('index.html', (err, data) => {
+      if (err) {
+        // Ошибка чтения файла
+        res.writeHead(404);
+        res.write('File not found!');
+      } else {
+        // Отправка HTML-страницы
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+      }
+      res.end();
+    });
+  });
+  
+  server.listen(3000, () => {
+    console.log('Server is running on port 3000');
+  });
